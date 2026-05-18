30b804a4c21773ad084d9707aeed3c7d89b8fbb6
Akil-Dikshan <akildikshan02@gmail.com>
2026-05-18 20:54:46 +0530

    feat(user): SB-35 add MentorProfile repository

--- user-service/src/main/java/com/skillbridge/user/repository/MentorProfileRepository.java
+++ user-service/src/main/java/com/skillbridge/user/repository/MentorProfileRepository.java
@@ -1,4 +1,12 @@
 package com.skillbridge.user.repository;
 
-public class MentorProfileRepository {
+import com.skillbridge.user.model.MentorProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface MentorProfileRepository extends JpaRepository<MentorProfile, Long> {
+    Optional<MentorProfile> findByUserId(Long userId);
+    boolean existsByUserId(Long userId);
 }
