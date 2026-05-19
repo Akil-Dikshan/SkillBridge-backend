981bd9b2c75ff77f687db3b44e17184173b1cd4a
Akil-Dikshan <akildikshan02@gmail.com>
2026-05-19 21:57:40 +0530

    feat(user): SB-41 add MentorAvailability repository

--- user-service/src/main/java/com/skillbridge/user/repository/MentorAvailabilityRepository.java
+++ user-service/src/main/java/com/skillbridge/user/repository/MentorAvailabilityRepository.java
@@ -1,4 +1,14 @@
 package com.skillbridge.user.repository;
 
-public interface MentorAvailabilityRepository {
+import com.skillbridge.user.model.MentorAvailability;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.DayOfWeek;
+import java.util.List;
+
+@Repository
+public interface MentorAvailabilityRepository extends JpaRepository<MentorAvailability, Long> {
+    List<MentorAvailability> findByUserId(Long userId);
+    List<MentorAvailability> findByUserIdAndDayOfWeek(Long userId, DayOfWeek dayOfWeek);
+    List<MentorAvailability> findByDayOfWeekAndActiveTrue(DayOfWeek dayOfWeek);
 }
