df253bf2ef952b919543ac6dc9eefae3841bb8e2
Akil-Dikshan <akildikshan02@gmail.com>
2026-05-19 21:57:36 +0530

    feat(user): SB-41 add MentorAvailability entity

---
+++ user-service/src/main/java/com/skillbridge/user/dto/AvailabilityRequest.java
@@ -0,0 +1,4 @@
+package com.skillbridge.user.dto;
+
+public class AvailabilityRequest {
+}
---
+++ user-service/src/main/java/com/skillbridge/user/repository/MentorAvailabilityRepository.java
@@ -0,0 +1,4 @@
+package com.skillbridge.user.repository;
+
+public interface MentorAvailabilityRepository {
+}
