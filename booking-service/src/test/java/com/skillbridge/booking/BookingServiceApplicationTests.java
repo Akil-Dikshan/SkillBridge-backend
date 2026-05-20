9a575e7cb60b9907dad0ba0b8ee4e16bcb6afdba
Akil-Dikshan <akildikshan02@gmail.com>
2026-05-21 00:25:12 +0530

    fix(booking-service): SB-60 mock UserServiceClient in context load test

--- booking-service/src/test/java/com/skillbridge/booking/BookingServiceApplicationTests.java
+++ booking-service/src/test/java/com/skillbridge/booking/BookingServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.skillbridge.booking;
 
+import com.skillbridge.booking.client.UserServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BookingServiceApplicationTests {
+
+	@MockBean
+	UserServiceClient userServiceClient;
 
 	@Test
 	void contextLoads() {
